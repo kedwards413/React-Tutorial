ab71c685b3f84d8b25b132769323dd4e23b0cbf9
kedwards413 <kaela.rose.edwards@gmail.com>
2021-01-04 21:45:25 +0000

    updates

--- exercises/05-adding-styles/index.js
+++ exercises/05-adding-styles/index.js
@@ -4,13 +4,16 @@ import PropTypes from "prop-types";
 
 //add the styles here
 const mySuperStyles = {
-
+	fontSize: "16px",
+	border: "1px solid yellow",
+	background: "black"
 };
 
 const Badge = props => {
 	return (
 		<button style={mySuperStyles} type="button" className="btn btn-primary">
-			{props.label}<span className="badge badge-light">{props.number}</span>
+			{props.label}
+			<span className="badge badge-light">{props.number}</span>
 		</button>
 	);
 };
